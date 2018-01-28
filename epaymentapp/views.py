0ca2cb2e0a5ea9b25fcf08a96313c8d91f9f665b
szymongor <gornioczek.szymon@gmail.com>
2018-01-28 13:23:22 +0100

    JWT Auth

--- epaymentapp/views.py
+++ epaymentapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 # Create your views here.
 from epaymentapp.models import Post
 from epaymentapp.serializers import PostSerializer
