6c7c89288bb2792d94f2ef503032286a22add7f2
szymongor <gornioczek.szymon@gmail.com>
2018-02-26 16:43:11 +0100

    Api - Adding posts

--- epaymentapp/views.py
+++ epaymentapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 # Create your views here.
-from epaymentapp.models import Post
+from epaymentapp.models import Post, SiteUser
 from epaymentapp.serializers import PostSerializer
 
 
@@ -12,3 +12,14 @@ class PostsList(APIView):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        request_post = request.data['post']
+        site_user = SiteUser.objects.get(user= request.user)
+        new_post = Post.objects.create(
+            author = site_user,
+            title = request_post['title'],
+            text = request_post['text']
+        )
+        new_post.save();
+        return Response(request_post)
