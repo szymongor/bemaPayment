aa5813ed76ff2ff600af09303fcff9c5762685a0
szymongor <gornioczek.szymon@gmail.com>
2018-03-05 10:58:17 +0100

    Payments and new Bill Admin Page

--- epaymentapp/admin.py
+++ epaymentapp/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Post, SiteUser, Bill
+from .models import Post, SiteUser, Bill, Payment
+
+
+class BillAdmin(admin.ModelAdmin):
+    # Klasa do wyświetlania płatności powiązanych z rachunkiem
+    class PaymentsInline(admin.TabularInline):
+        model = Payment
+        extra = 1 #Tylko jeden form do dodawania nowej płatności, domyślnie są 3 (lol)
+
+    inlines = [
+        PaymentsInline,
+    ]
+
 
 admin.site.register(SiteUser)
 admin.site.register(Post)
-admin.site.register(Bill)
+admin.site.register(Bill, BillAdmin)
+admin.site.register(Payment)
--- epaymentapp/models.py
+++ epaymentapp/models.py
@@ -33,12 +33,22 @@ class Bill(models.Model):
         ('BLOCKED', 'Zawieszony'),
     )
 
-    title =  models.CharField(max_length=30)
+    title =  models.CharField(max_length=30, unique=True)
     type = models.CharField(max_length=20, choices=type_choices)
+    amount = models.PositiveIntegerField()
     founder = models.ForeignKey('SiteUser', on_delete=models.CASCADE,
     related_name='fouded_bills')
     obligors = models.ManyToManyField('SiteUser', related_name='bills_to_pay')
     status = models.CharField (max_length=20, choices=status_choices)
 
+
     def __str__(self):
         return self.title
+
+class Payment(models.Model):
+    amount = models.PositiveIntegerField()
+    bill = models.ForeignKey('Bill', related_name='payments', on_delete=models.CASCADE,)
+    founder = models.ForeignKey('SiteUser', on_delete=models.CASCADE,
+                                related_name='payments')
+    created_date = models.DateTimeField(
+        default=timezone.now)
